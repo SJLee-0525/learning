c8117c9444e93fffcebde4c458d1faace98a2dee
sungjoon111 <sungjoon25@yonsei.ac.kr>
2024-09-04 19:59:14 +0900

    240904-h01

--- algorithm/day_20/swea_10966.py
+++ algorithm/day_20/swea_10966.py
@@ -1,35 +1,28 @@
 from collections import deque
 
-def find_water(N, M):
+def bfs():
+    Q = deque()
+
+    # 큐에 주변에 L을 가진 W의 좌표를 저장
     for i in range(N):
         for j in range(M):
             if beach[i][j] == 'W':
                 for k in range(4):
                     mi, mj = i + di[k], j + dj[k]
+                    # 만약 주변이 전부 W라면 그거보다 육지에 가까운 W가 존재하기에 담을 필요 없을듯
                     if 0 <= mi < N and 0 <= mj < M and beach[mi][mj] == 'L':
-                        bfs(i, j)
+                        Q.append((i, j))
+                        distance[i][j] = 1  # 거리 표시도 지금 미리 해둠 (마지막에 -1 해줘야함)
                         break
 
-def bfs(si, sj):
-    checked = [[0] * M for _ in range(N)]
-    Q = deque()
-
-    distance[si][sj] = 1
-    checked[si][sj] = 1
-    Q.append((si, sj))
+    # BFS
     while Q:
         i, j = Q.popleft()
         for k in range(4):
             mi, mj = i + di[k], j + dj[k]
-            if 0 <= mi < N and 0 <= mj < M and beach[mi][mj] != 'W' and checked[mi][mj] == 0:
+            if 0 <= mi < N and 0 <= mj < M and distance[mi][mj] == 0:
                 Q.append((mi, mj))
-                checked[mi][mj] = 1
-                temp_distance = distance[i][j] + 1
-                if not distance[mi][mj]:
-                    distance[mi][mj] = temp_distance
-                elif distance[mi][mj]:
-                    if distance[mi][mj] > temp_distance:
-                        distance[mi][mj] = temp_distance
+                distance[mi][mj] = distance[i][j] + 1
 
 
 def cal_distance(N, M):
@@ -37,17 +30,19 @@ def cal_distance(N, M):
     for i in range(N):
         for j in range(M):
             if beach[i][j] == 'L':
-                s += distance[i][j] - 1
+                s += distance[i][j] - 1 # distance에 저장된 값보다 1 적게 계산해야 함
     return s
 
+#####################################################################################
+
 for tc in range(1, int(input()) + 1):
-    N, M = map(int, input().split()) # N 세로, M 가로
+    N, M = map(int, input().split())    # N 세로, M 가로
     beach = [list(input()) for _ in range(N)]
     distance = [[0] * M for _ in range(N)]
 
     di = [1, 0, -1, 0]
     dj = [0, 1, 0, -1]
 
-    find_water(N, M)
-    # print(distance)
+    bfs()
+
     print(f'#{tc} {cal_distance(N, M)}')
